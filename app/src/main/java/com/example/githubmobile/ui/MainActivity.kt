20355964853f2bd249d7fe5d766780e5137fdb4c
Vitor <nun720@gmail.com>
2020-09-06 12:58:31 -0300

    Adicionadas dependencias

---
+++ app/src/main/java/com/example/githubmobile/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.githubmobile.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.githubmobile.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
